0fbfb5417b192b710e6949c4db24107965170a6f
olesya <nearolesya@gmil.com>
2023-08-06 20:05:21 +0300

    задание4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int imput = 1; imput < number; imput++)
+{
+if (imput % 2 == 0)
+
+System.Console.Write(imput + ", ");
+}
