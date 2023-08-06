5538fe91f0139508ef0f4f694dec03199eb3d4d3
olesya <nearolesya@gmil.com>
2023-08-06 19:45:02 +0300

    задание3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введи число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 2 == 0)
+{
+Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
