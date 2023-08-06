059ad2aea4038107412814b415d409d32abd04e3
olesya <nearolesya@gmil.com>
2023-08-06 19:19:59 +0300

    задание2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Первое число");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число");
+
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Третье число");
+
+int LastNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+
+if (SecondNumber > max) max = SecondNumber;
+
+if (LastNumber > max) max = LastNumber;
+
+Console.WriteLine(max);
