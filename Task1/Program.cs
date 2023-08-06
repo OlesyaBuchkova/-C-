5345b3dafddbd7e0e3c2ead3f4cea83d5d7c6bc2
olesya <nearolesya@gmil.com>
2023-08-06 18:48:06 +0300

    задание1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Первое число");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Второе число");
+
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > SecondNumber)
+
+{
+    System.Console.WriteLine (firstNumber);
+}
+else
+{
+   System.Console.WriteLine (SecondNumber); 
+}
